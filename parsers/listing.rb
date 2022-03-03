83dfa1091b3622f327ae99068c62e4bf001a7fc2
Riyan Wibowo <tunggalrahayu1@gmail.com>
2022-03-04 00:28:49 +0700

    get all data

--- parsers/listing.rb
+++ parsers/listing.rb
@@ -21,21 +21,25 @@ products.each do |product|
    
 end
 
-total_page_summary = nokogiri.css(".list-pagination")
-# total_page = total_page_summary.scan(/\d+/).first.to_i
-p total_page_summary
-# 2.upto(10) do |i|
-#     if i < 11
-#         url = "https://www.aliexpress.com/category/100003109/women-clothing.html?page=#{i}"
-#         pages << {
-#             method: "GET",
-#             fetch_type: "browser",
-#             headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-#             vars: {
-#                 category: "Women's clothing"
-#             },
-#             url:url,
-#             page_type: "listings"
-#         }
-#     end
-# end
+total_page_summary = nokogiri.css(".total-page").text
+total_page = total_page_summary.scan(/\d+/).first.to_i
+
+2.upto(total_page) do |i|
+    if i < 11
+        url = "https://www.aliexpress.com/category/100003109/women-clothing.html?page=#{i}"
+        pages << {
+            method: "GET",
+            fetch_type: "browser",
+            headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+            vars: {
+                category: "Women's clothing"
+            },
+            url:url,
+            page_type: "listings",
+            display: {
+                "width": 1920,
+                "height": 3300
+            }
+        }
+    end
+end
